2add99546fa5c3f615e568e7149a95e09fd73e70
prajaktay23 <prajaktayadav525@gmail.com>
2019-06-20 16:19:53 +0530

    calc

---
+++ app/src/main/java/com/abc/calc/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.abc.calc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etnum1 = findViewById<EditText>(R.id.etnum1)
+
+        val etnum2 = findViewById<EditText>(R.id.etnum2)
+
+        val btadd = findViewById<Button>(R.id.btadd)
+
+        val btsub = findViewById<Button>(R.id.btsub)
+
+        val btmul = findViewById<Button>(R.id.btmul)
+
+        val btdiv = findViewById<Button>(R.id.btdiv)
+
+        val TxSt = findViewById<TextView>(R.id.TxSt)
+
+        btadd.setOnClickListener {
+            TxSt.text=""+ (Integer.parseInt(etnum1.text.toString())+ Integer.parseInt(etnum2.text.toString()))
+        }
+        btsub.setOnClickListener {
+            TxSt.text=""+ (Integer.parseInt(etnum1.text.toString())- Integer.parseInt(etnum2.text.toString()))
+        }
+        btmul.setOnClickListener {
+            TxSt.text=""+ (Integer.parseInt(etnum1.text.toString())* Integer.parseInt(etnum2.text.toString()))
+        }
+        btdiv.setOnClickListener {
+            TxSt.text=""+ (Integer.parseInt(etnum1.text.toString())/ Integer.parseInt(etnum2.text.toString()))
+        }
+
+
+    }
+
+}
