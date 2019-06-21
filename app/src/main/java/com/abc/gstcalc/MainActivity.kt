93403b3acae2a71c6c7faed5f4c16b50252c0b86
prajaktay23 <prajaktayadav525@gmail.com>
2019-06-21 11:34:06 +0530

    gst calc

---
+++ app/src/main/java/com/abc/gstcalc/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.abc.gstcalc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val edamt = findViewById<EditText>(R.id.edamt)
+
+        val edgst = findViewById<EditText>(R.id.edgst)
+
+        val txtans = findViewById<TextView>(R.id.txtans)
+
+        val btcal = findViewById<Button>(R.id.btcal)
+
+        val txtrs = findViewById<TextView>(R.id.txtrs)
+
+        btcal.setOnClickListener {
+
+            txtans.text =
+                "" + (Integer.parseInt(edamt.text.toString()) + Integer.parseInt(edamt.text.toString()) * Integer.parseInt(
+                    edgst.text.toString()
+                ) / 100)
+            txtrs.text = "" + (Integer.parseInt(txtans.text.toString()) - Integer.parseInt(edamt.text.toString()))
+        }
+    }
+}
